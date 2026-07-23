d76700441a73b20b07fd81f1c7f6c365c0cb288a
Fatima-zahra Bendnane <fbendnan@c3r7p9.1337.ma>
2026-07-23 14:12:19 +0100

    add

--- src/__main__.py
+++ src/__main__.py
@@ -13,30 +13,93 @@ model = Small_LLM_Model()
 def chat(prompt, max_tokens=100):
     ids = model.encode(prompt).tolist()[0]
     output = ""
-    open_brace_id = model.encode("{").tolist()[0][0]
-    quote_id = model.encode('"').tolist()[0][0]
-    
+    # open_brace_id = model.encode("{").tolist()[0][0]
+    # quote_id = model.encode('"').tolist()[0][0]
+    pos = 0
+    my_json_structure = [
+        ("forced", '{'),
+        ("forced", ' '),
+        ("forced", '"'),
+        ("forced", "prompt"),
+        ("forced", '"'),
+        ("forced", ':'),
+        ("not_forced", "prompt_value"),
+        ("forced", ','),
+        ("forced", ' '),
+        ("forced", '"'),
+        ("forced", "name"),
+        ("forced", '"'),
+        ("forced", ':'),
+        ("not_forced", "fun_name"),
+        ("forced", ','),
+        ("forced", " "),
+        ("forced", '"'),
+        ("forced", "parameters"),
+        ("forced", '"'),
+        ("forced", ":"),
+        ("forced", " "),
+        ("forced", "{"),
+        ("not_forced", "param_values"),
+        ("forced", "}"),
+        ("forced", "}"),
+    ]
+
     for _ in range(max_tokens):
         logits = model.get_logits_from_input_ids(ids)
 
-        if output == "":
-            for token_id in range(len(logits)):
-                if token_id != open_brace_id:
-                    logits[token_id] = float('-inf')
-
-        elif output == "{":
-            for token_id in range(len(logits)):
-                    if token_id != quote_id:
+        part_type, part_token = my_json_structure[pos]
+        if pos < len(my_json_structure) and part_type == 'forced':
+            token_pos = 0
+            tok_id = model.encode(part_token).tolist()[0]
+            len_tok = len(tok_id)
+            while len_tok > token_pos:
+                tok_now = tok_id[token_pos]
+                for token_id in range(len(logits)):
+                    if token_id != tok_now:
                         logits[token_id] = float('-inf')
+                next_id = int(np.argmax(logits))
+                token = model.decode([next_id])
+                output += token
+                token_pos += 1
+                ids.append(next_id)
+            pos += 1
+            if pos == len(my_json_structure):
+                break
 
-        next_id = int(np.argmax(logits))
-        token = model.decode([next_id])
-        # print("token bera")
+        elif pos < len(my_json_structure) and part_type == 'not_forced':
+            if part_token == "prompt_value":
+                ...
+            pos += 1
+            
+            for _ in range(10):
+                next_id = int(np.argmax(logits))
+                token = model.decode([next_id])
+                output += token
+                ids.append(next_id)
+                logits = model.get_logits_from_input_ids(ids)
         if token in ["<|im_end|>", "</s>", "<|endoftext|>"]:
             break
-            # print(token)
-        output += token
-        ids.append(next_id)
+
+
+
+            # elif part_token == "fun_name":
+            #     ...
+            # elif part_token == "param_values":
+            #     ...
+
+
+        # next_id = int(np.argmax(logits))
+        # token = model.decode([next_id])
+        # # print("token bera")
+        # if token in ["<|im_end|>", "</s>", "<|endoftext|>"]:
+        #     break
+        #     # print(token)
+        # output += token
+        
+        # pos += 1
+        # if pos == len(my_json_structure):
+        #     break
+        # ids.append(next_id)
     
     return output
 
@@ -49,6 +112,6 @@ with open("data/input/functions_definition.json") as f:
     func = json.load(f)
     # print(func)
 
-print(chat(f"prompt : {prompt[7]['prompt']}, functions: {func},"
+print(chat(f"prompt : {prompt[9]['prompt']}, functions: {func},"
             "based on the prompt chose the right function to it and give me just a JSON "
             "output (no extra text) with informations like prompt, name (of function used), parameters"))
